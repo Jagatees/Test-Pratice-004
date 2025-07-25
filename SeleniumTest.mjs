ddeb212af4052a87e831f5f065d871838a367f95
Jagatees <2301770@sit.singaporetech.edu.sg>
2025-07-25 15:39:07 +0800

    Checkpoint

--- SeleniumTest.mjs
+++ SeleniumTest.mjs
@@ -14,6 +14,7 @@ const seleniumUrl = environment === 'github'
 const serverUrl = environment === 'github'
   ? 'http://testserver:3000'
   : 'http://host.docker.internal:3000';
+  
 
 console.log(`Running tests in '${environment}' environment`);
 console.log(`Selenium URL: ${seleniumUrl}`);
